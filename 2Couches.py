3da3284855487bcdf27a2ba31fbbd7932fafcd36
Benoit P <43702903+BenLipS@users.noreply.github.com>
2022-03-14 22:28:14 -0400

    Add files via upload

---
+++ 2Couches.py
@@ -0,0 +1,107 @@
+from sklearn.datasets import make_blobs, make_circles
+from ANNUtils import *
+
+#first exemple for NN 2 Layers
+def execFirstExample():
+    X, y = make_circles(n_samples=100, noise=0.1, factor=0.3, random_state=0)
+    X = X.T
+    y = y.reshape((1, y.shape[0]))
+
+    print('dimensions de X:', X.shape)
+    print('dimensions de y:', y.shape)
+
+    plt.scatter(X[0, :], X[1, :], c=y, cmap='summer')
+    plt.show()
+
+    neural_network2(X, y, n1=32)
+
+#execFirstExample()
+
+#Cat and dogs example
+
+from utilities import *
+
+def execCatsDogs():
+    
+    X_train, y_train, X_test, y_test = load_data()
+
+    print(X_train.shape)
+    print(y_train.shape)
+    print(np.unique(y_train, return_counts=True))
+
+    print(X_test.shape)
+    print(y_test.shape)
+    print(np.unique(y_test, return_counts=True))
+
+    plt.figure(figsize=(16, 8))
+    for i in range(1, 10):
+        plt.subplot(4, 5, i)
+        plt.imshow(X_train[i], cmap='gray')
+        plt.title(y_train[i])
+        plt.tight_layout()
+    plt.show()
+
+
+    X_train_reshape = X_train.reshape(X_train.shape[0], -1) / X_train.max()
+    X_train_reshape = X_train_reshape.T
+    X_train_reshape = normalize( X_train_reshape )
+
+    X_test_reshape = X_test.reshape(X_test.shape[0], -1) / X_test.max()
+    X_test_reshape = X_test_reshape.T
+    X_test_reshape = normalize( X_test_reshape )
+
+    #X_train_reshape_norm = normalize( X_train_reshape)
+
+    print("y-train shape = ", y_train.shape )
+    y_train_reshape = y_train.T
+    y_test_reshape = y_test.T
+
+    m_train = 300
+    m_test = 80
+    #X_test_reshape = X_test_reshape[:,:m_test]
+    #X_train_reshape = X_train_reshape[:,:m_train]
+    #y_test_reshape = y_test_reshape[:,:m_test]
+    #y_train_reshape = y_train_reshape[:,:m_train]
+
+
+    print('dimensions de X_train_reshape:', X_train_reshape.shape)
+    print('dimensions de y_train_reshape:', y_train_reshape.shape)
+    print('dimensions de X_test_reshape:', X_test_reshape.shape)
+    print('dimensions de y_test_reshape:', y_test_reshape.shape)
+
+    #params = neural_network2(X_train_reshape, y_train_reshape, X_test_reshape, y_test_reshape, n1 = 128, learning_rate = 0.001, n_iter=80000, printTest = True)
+    params = neural_network(X_train_reshape, y_train_reshape, X_test_reshape, y_test_reshape, neuronnesinternes = [ 16, 32, 32, 16 ], learning_rate = 0.05, n_iter=500000, printTest = True)
+
+execCatsDogs()
+
+
+#Generic Neural Network
+def testGenericNeuralNetwork():
+    neuronnes = [ 2, 2, 2, 2, 2, 2, 32, 1]
+
+    X, y = make_circles(n_samples=100, noise=0.1, factor=0.3, random_state=0)
+    X = X.T
+    y = y.reshape((1, y.shape[0]))
+
+    print('dimensions de X:', X.shape)
+    print('dimensions de y:', y.shape)
+
+    parametres = initialisation( neuronnes )
+    print( "parametres : ", parametres )
+
+    activation = forward_propagation( X, parametres ) 
+    print( "activation : ", activation )
+
+    gradients = back_propagation( X, y, parametres, activation )
+    print( "gradients : ", gradients )
+
+    parametres = update( gradients, parametres, 0.01 )
+    print( "parametres : ", parametres )
+
+    reponse = predict( X, parametres )
+    print( "reponse : ", reponse )
+
+    neural_network(X, y, X_test = 0, y_test = 0, neuronnesinternes = [ 32, 16, 32, 16, 128 ], learning_rate = 0.1, n_iter = 10000, printTest = False)
+
+#testGenericNeuralNetwork()
+
---
+++ ANNUtils.py
@@ -0,0 +1,309 @@
+from re import L
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.metrics import accuracy_score, log_loss
+from tqdm import tqdm
+
+def initialisation2(n0, n1, n2):
+
+    W1 = np.random.randn(n1, n0)
+    b1 = np.zeros((n1, 1))
+    W2 = np.random.randn(n2, n1)
+    b2 = np.zeros((n2, 1))
+
+    parametres = {
+        'W1': W1,
+        'b1': b1,
+        'W2': W2,
+        'b2': b2
+    }
+
+    return parametres
+
+def initialisation( neuronnes ):
+    parametresW = []
+    parametresb = []
+    for i in range( 1, len( neuronnes ) ):
+        parametresW.append( np.random.randn( neuronnes[ i ], neuronnes[ i - 1 ] ) )
+        parametresb.append( np.zeros( ( neuronnes[ i ], 1 ) ) )
+    
+    parametres = {
+        'parametresW' : parametresW,
+        'parametresb' : parametresb
+    }
+
+    return parametres
+
+def forward_propagation2(X, parametres):
+
+    W1 = parametres['W1']
+    b1 = parametres['b1']
+    W2 = parametres['W2']
+    b2 = parametres['b2']
+
+    Z1 = W1.dot(X) + b1
+    A1 = 1 / (1 + np.exp(-Z1))
+
+    Z2 = W2.dot(A1) + b2
+    A2 = 1 / (1 + np.exp(-Z2))
+
+    activations = {
+        'A1': A1,
+        'A2': A2
+    }
+
+    return activations
+
+def forward_propagation(X, parametres):
+
+    W = parametres[ 'parametresW' ]
+    b = parametres[ 'parametresb' ]
+
+    activations = []
+
+    Z1 = W[ 0 ].dot(X) + b[ 0 ]
+    A1 = 1 / (1 + np.exp(-Z1))
+    activations.append( A1 )
+
+    for i in range( 1, len( W ) ):
+        Z = W[ i ].dot( activations[ i - 1 ]) + b[ i ]
+        A = 1 / (1 + np.exp(-Z))
+        activations.append( A )
+
+    return activations
+
+
+def back_propagation2(X, y, parametres, activations):
+
+    A1 = activations['A1']
+    A2 = activations['A2']
+    W2 = parametres['W2']
+
+    m = y.shape[1]
+
+    dZ2 = A2 - y
+    dW2 = 1 / m * dZ2.dot(A1.T)
+    db2 = 1 / m * np.sum(dZ2, axis=1, keepdims = True)
+
+    dZ1 = np.dot(W2.T, dZ2) * A1 * (1 - A1)
+    dW1 = 1 / m * dZ1.dot(X.T)
+    db1 = 1 / m * np.sum(dZ1, axis=1, keepdims = True)
+
+    gradients = {
+        'dW1' : dW1,
+        'db1' : db1,
+        'dW2' : dW2,
+        'db2' : db2
+    }
+    
+    return gradients
+
+def back_propagation(X, y, parametres, activations):
+
+    A = activations
+    W = parametres['parametresW']
+
+    m = y.shape[1]
+    
+    gradientsdW = []
+    gradientsdb = []
+
+    dZ = A[ len( W ) - 1 ] - y
+    dW = 1 / m * dZ.dot(A[ len( W ) - 2 ].T)
+    db = 1 / m * np.sum(dZ, axis=1, keepdims = True)
+    gradientsdW.insert(0, dW)
+    gradientsdb.insert(0, db)
+
+    for i in range( 1, len( W ) - 1 ):
+        dZ = np.dot(W[ len( W ) - i ].T, dZ) * A[ len( W ) - i - 1 ] * ( 1 - A[ len( W ) - i - 1 ] )
+        dW = 1 / m * dZ.dot(A[ len( W ) - i - 2].T)
+        db = 1 / m * np.sum(dZ, axis=1, keepdims = True)
+        gradientsdW.insert(0, dW)
+        gradientsdb.insert(0, db)
+
+    dZ = np.dot(W[ 1 ].T, dZ) * A[ 0 ] * ( 1 - A[ 0 ] )
+    dW = 1 / m * dZ.dot(X.T)
+    db = 1 / m * np.sum(dZ, axis=1, keepdims = True)
+    gradientsdW.insert(0, dW)
+    gradientsdb.insert(0, db)
+
+    gradients = {
+        'gradientsdW' : gradientsdW,
+        'gradientsdb' : gradientsdb
+    }
+    
+    return gradients
+
+def update2(gradients, parametres, learning_rate):
+
+    W1 = parametres['W1']
+    b1 = parametres['b1']
+    W2 = parametres['W2']
+    b2 = parametres['b2']
+
+    dW1 = gradients['dW1']
+    db1 = gradients['db1']
+    dW2 = gradients['dW2']
+    db2 = gradients['db2']
+
+    W1 = W1 - learning_rate * dW1
+    b1 = b1 - learning_rate * db1
+    W2 = W2 - learning_rate * dW2
+    b2 = b2 - learning_rate * db2
+
+    parametres = {
+        'W1': W1,
+        'b1': b1,
+        'W2': W2,
+        'b2': b2
+    }
+
+    return parametres
+
+def update(gradients, parametres, learning_rate):
+    W = parametres[ 'parametresW' ]
+    b = parametres[ 'parametresb' ]
+    dW = gradients[ 'gradientsdW' ]
+    db = gradients[ 'gradientsdb' ]
+
+    parametresW = []
+    parametresb = []
+
+    for i in range( 0, len( W ) ):
+        Wn = W[ i ] - learning_rate * dW[ i ]
+        bn = b[ i ] - learning_rate * db[ i ]
+        parametresW.append( Wn )
+        parametresb.append( bn )
+
+    parametres = {
+        'parametresW' : parametresW,
+        'parametresb' : parametresb
+    }
+
+    return parametres
+
+def predict2(X, parametres):
+  activations = forward_propagation2(X, parametres)
+  A2 = activations['A2']
+  return A2 >= 0.5
+
+def predict(X, parametres):
+  activations = forward_propagation(X, parametres)
+  Af = activations[ -1 ]
+  return Af >= 0.5
+
+def neural_network2(X, y, X_test = 0, y_test = 0, n1=32, learning_rate = 0.1, n_iter = 1000, printTest = False):
+
+    # initialisation parametres
+    n0 = X.shape[0]
+    n2 = y.shape[0]
+    np.random.seed(0)
+    parametres = initialisation2(n0, n1, n2)
+
+    train_loss = []
+    train_acc = []
+    history = []
+
+    loss_test = []
+    acc_test = []
+
+    # gradient descent
+    for i in tqdm(range(n_iter)):
+        activations = forward_propagation2(X, parametres)
+        gradients = back_propagation2(X, y, parametres, activations)
+        parametres = update2(gradients, parametres, learning_rate)
+
+        if i % 10 == 0:
+            # Plot courbe d'apprentissage
+            train_loss.append(log_loss(y.flatten(), activations['A2'].flatten()))
+            y_pred = predict2(X, parametres)
+            train_acc.append(accuracy_score(y.flatten(), y_pred.flatten()))
+
+            #Test
+            if printTest:
+                activations_test = forward_propagation2( X_test, parametres )
+                A2_test = activations_test[ "A2" ]
+                loss_test.append( log_loss( y_test.flatten(), A2_test.flatten() ) )
+                y_pred = predict2( X_test, parametres )
+                acc_test.append( accuracy_score( y_test.flatten(), y_pred.flatten() ) )
+            
+            history.append([parametres.copy(), train_loss, train_acc, i])
+
+
+    plt.figure(figsize=(12, 4))
+    plt.subplot(1, 2, 1)
+    plt.plot(train_loss, label='train loss')
+    if printTest:
+        plt.plot( loss_test, label = 'test loss' )
+    plt.legend()
+    plt.subplot(1, 2, 2)
+    plt.plot(train_acc, label='train acc')
+    if printTest:
+        plt.plot( acc_test, label = 'test acc' )
+    plt.legend()
+    plt.show()
+
+    return parametres
+
+def neural_network(X, y, X_test = 0, y_test = 0, neuronnesinternes = [ 32 ], learning_rate = 0.1, n_iter = 1000, printTest = False):
+
+    # initialisation parametres
+    n0 = X.shape[0]
+    neuronnes = neuronnesinternes 
+    neuronnes.insert( 0, n0 )
+    n2 = y.shape[0]
+    neuronnes.append( n2 )
+    np.random.seed(0)
+    parametres = initialisation( neuronnes )
+
+    train_loss = []
+    train_acc = []
+    history = []
+
+    loss_test = []
+    acc_test = []
+
+    # gradient descent
+    for i in tqdm(range(n_iter)):
+        activations = forward_propagation( X, parametres )
+        gradients = back_propagation( X, y, parametres, activations )
+        parametres = update( gradients, parametres, learning_rate )
+
+        if i % 10 == 0:
+            # Plot courbe d'apprentissage
+            train_loss.append(log_loss(y.flatten(), activations[ -1 ].flatten()))
+            y_pred = predict(X, parametres)
+            train_acc.append(accuracy_score(y.flatten(), y_pred.flatten()))
+
+            #Test
+            if printTest:
+                activations_test = forward_propagation( X_test, parametres )
+                Af_test = activations_test[ -1 ]
+                loss_test.append( log_loss( y_test.flatten(), Af_test.flatten() ) )
+                y_pred = predict( X_test, parametres )
+                acc_test.append( accuracy_score( y_test.flatten(), y_pred.flatten() ) )
+            
+            history.append([parametres.copy(), train_loss, train_acc, i])
+
+
+    plt.figure(figsize=(12, 4))
+    plt.subplot(1, 2, 1)
+    plt.plot(train_loss, label='train loss')
+    if printTest:
+        plt.plot( loss_test, label = 'test loss' )
+    plt.legend()
+    plt.subplot(1, 2, 2)
+    plt.plot(train_acc, label='train acc')
+    if printTest:
+        plt.plot( acc_test, label = 'test acc' )
+    plt.legend()
+    plt.show()
+
+    return parametres
+
+def normalize( X ):
+    X_trainNorm = X.astype(np.double)
+    for i in range(X_trainNorm.shape[0]):
+        for x in range(X_trainNorm.shape[1]):
+            X_trainNorm[i][x] = X_trainNorm[i][x] / 255
+    return X_trainNorm
